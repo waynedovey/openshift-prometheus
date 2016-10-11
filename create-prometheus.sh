7347a9f7ef66bf6810382658654c61daa8e99cc9
root <root@m0.sit-111.ocp.bfsonlinebanking.syd.non.c1.macquarie.com>
2016-10-12 10:00:51 +1100

    update

--- create-prometheus.sh
+++ create-prometheus.sh
@@ -4,6 +4,8 @@ oc project prometheus
 sleep 5
 
 oc create -f prometheus-dc.yaml
+#oadm policy add-cluster-role-to-user cluster-reader system:serviceaccount:prometheus:default
+oadm policy add-cluster-role-to-user cluster-reader system:serviceaccount:prometheus:metrics
 oc create -f prometheus-service.yaml 
 oc create -f prometheus-configmap.yaml
 oc create -f sa-metrics.yaml
