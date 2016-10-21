92b99f8d21c51c084cd059ef1cf4b39cd64390e9
root <root@m0.uat-47.ocp.bfsonlinebanking.syd.non.c1.macquarie.com>
2016-10-21 16:06:39 +1100

    update

--- create-grafana.sh
+++ create-grafana.sh
@@ -3,8 +3,8 @@ oadm new-project prometheus --node-selector="region=infra"
 oc project prometheus 
 sleep 5
 
-oc create -f grafana-dc.yaml
-oc create -f grafana-service.yaml 
+#oc create -f grafana-dc.yaml
+#oc create -f grafana-service.yaml 
 #oc create -f grafana-configmap.yaml
-oc create -f sa-metrics.yaml
+oc new-app --name='grafana' --strategy='docker' .
 oc expose service grafana
