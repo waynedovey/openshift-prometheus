f000bd2a0d38db2a92d3e813020dd90c7c79ebf0
root <root@m0.uat-48.ocp.bfsonlinebanking.syd.non.c1.macquarie.com>
2016-10-25 00:03:01 +1100

    update

--- create-prometheus.sh
+++ create-prometheus.sh
@@ -15,3 +15,5 @@ oadm pod-network make-projects-global nft
 oadm pod-network make-projects-global wow
 oadm pod-network make-projects-global prometheus
 oadm pod-network make-projects-global openshift-infra
+
+./prometheus-pvc-pv.sh
