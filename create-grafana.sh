24b034e7f418e7d52d9d073148ace7877b9f6f17
waynedovey <wdovey@gmail.com>
2017-04-04 18:21:17 +1000

    Update create-grafana.sh

--- create-grafana.sh
+++ create-grafana.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-source /etc/profile 
-export DEPLOY_DNS_DOMAIN=`/usr/local/bin/pipeline_dns -c ''`
+#source /etc/profile 
+#export DEPLOY_DNS_DOMAIN=`/usr/local/bin/pipeline_dns -c ''`
 
-oadm new-project prometheus --node-selector="region=infra"
+#oc adm new-project prometheus --node-selector="region=infra"
+oc adm new-project prometheus --node-selector="region=infra"
 oc project prometheus 
 sleep 5
 
 rm -fr gluster-ose
 oc new-app --name='grafana' --strategy='docker' .
-oc expose service grafana --hostname="grafana.${DEPLOY_DNS_DOMAIN}"
+#oc expose service grafana --hostname="grafana.${DEPLOY_DNS_DOMAIN}"
+oc expose service grafana
